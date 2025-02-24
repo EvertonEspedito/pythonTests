d4c10d7ad61ed5de366e981bddb28c9547c193d9
Everton Espedito <evertondev505@gmail.com>
2025-02-24 20:28:46 -0300

    Atualização
    Organização de Pastas

---
+++ pythonFic/TAREFAFICPY-M5/arquivoM5.py
@@ -0,0 +1,12 @@
+# Estudo de Caso: Gerenciador de Lista de Tarefas
+# Contexto: Você foi contratado para desenvolver um programa simples em Python que permita aos usuários gerenciar uma lista de tarefas. As tarefas devem ser armazenadas em um arquivo de texto, e o programa deve permitir que o usuário adicione novas tarefas, visualize todas as tarefas existentes e limpe a lista de tarefas.
+# Requisitos do Programa:
+# Adicionar Tarefa:
+# O usuário deve poder adicionar uma nova tarefa à lista.
+# A tarefa deve ser salva em um arquivo chamado tarefas.txt.
+# Visualizar Tarefas:
+# O usuário deve poder visualizar todas as tarefas armazenadas no arquivo.
+# Limpar Lista de Tarefas:
+# O usuário deve poder apagar todas as tarefas do arquivo.
+# Sair:
+# O programa deve permitir que o usuário saia do sistema.
