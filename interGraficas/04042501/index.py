c7896783594ade21b958be3ae1237f192dbd9e50
Everton Espedito <evertondev505@gmail.com>
2025-04-05 00:13:17 -0300

    update do flet ToDo list

--- interGraficas/04042501/index.py
+++ interGraficas/04042501/index.py
@@ -1,17 +1,36 @@
 import flet as ft
+import sqlite3
 
 class ToDo:
     def __init__(self, page:ft.Page):
         self.page = page
-        self.page.window.height = 650
-        self.page.window.width = 440
-        self.page.window.resizable = False
+        self.page.window.min_height = 650
+        self.page.window.min_width = 440
+        self.page.window.resizable = True
         self.page.window.always_on_top = True
         self.page.title = "Lista de Tarefas"
         self.page.bgcolor = "#4F4F4F"
+        self.db_execute('CREATE TABLE IF NOT EXISTS tasks(name,status)')
         self.page.update() # para aplicar as mudanças
 
         self.main_page()
+
+    def db_execute(self,query,params = []):
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute(query,params)
+            con.commit()
+            return cur.fetchall()
+        
+    def task_container(self):
+        return ft.Container(
+            height=self.page.height *0.8, #tamanho do container vai ser = 80%
+            content=ft.Column(
+                controls=[
+                    ft.Checkbox(label="Tarefa 1",value= True)
+                ]
+            )
+        )    
     def main_page(self):
         input_task = ft.TextField(hint_text="Digite uma Tarefa",expand=True,text_size=20,fill_color="#1C1C1C")
 
@@ -33,6 +52,7 @@ class ToDo:
             ],
         )
 
-        self.page.add(input_bar,tabs)
+        tasks = self.task_container()
+        self.page.add(input_bar,tabs, tasks)
         
 ft.app(target= ToDo)        
