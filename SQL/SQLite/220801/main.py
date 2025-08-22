c14dc8495881e757a6603b6799f90f9b43d9dc4e
Everton Espedito <evertondev505@gmail.com>
2025-08-22 11:24:37 -0300

    SQLite intro

--- SQL/SQLite/220801/main.py
+++ SQL/SQLite/220801/main.py
@@ -2,3 +2,21 @@ import _sqlite3
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
+DB_NAME = 'minha_db.sqlite3'
+DB_PATH = BASE_DIR / DB_NAME
+TABLE_NAME = 'clientes'
+
+coneccao = _sqlite3.connect(DB_PATH)# Conexão com o banco de dados
+cursor = coneccao.cursor() # Criação do cursor
+
+#SQl para criar a tabela
+cursor.execute(f'''
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        peso REAL NOT NULL
+    );
+''')
+
+cursor.close() # Fechamento do cursor
+coneccao.close() # Fechamento da conexão com o banco de dados
