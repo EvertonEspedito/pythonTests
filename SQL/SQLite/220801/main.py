efbceb754bf23bdbc1598f7b461d906cc7114d73
Everton Espedito <evertondev505@gmail.com>
2025-08-25 11:52:03 -0300

    SQLite
    INSERT, CREATE, DELETE

--- SQL/SQLite/220801/main.py
+++ SQL/SQLite/220801/main.py
@@ -13,10 +13,26 @@ cursor = coneccao.cursor() # Criação do cursor
 cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        peso REAL NOT NULL
+        name TEXT NOT NULL,
+        weight REAL NOT NULL
     );
 ''')
+coneccao.commit() # Confirmação da criação da tabela
+
+# SQL para inserir dados na tabela
+cursor.execute(f'''
+    INSERT INTO {TABLE_NAME} (name, weight) VALUES
+    ('Maria', 60.5),
+    ('João', 75.0),
+    ('Ana', 55.3);
+''')
+coneccao.commit() # Confirmação da inserção dos dados
+
+# SQL deleTe para remover um registro
+cursor.execute(f'''
+    DELETE FROM {TABLE_NAME} WHERE name = 'João';
+''')
+coneccao.commit() # Confirmação da deleção do registro
 
 cursor.close() # Fechamento do cursor
 coneccao.close() # Fechamento da conexão com o banco de dados
